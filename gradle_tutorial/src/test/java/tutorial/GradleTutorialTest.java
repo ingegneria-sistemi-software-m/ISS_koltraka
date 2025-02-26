5d566a956496220302d979de3df815758c134579
kevin <kevinkoltraka1011@gmail.com>
2025-02-26 16:11:18 +0100

    imparato ad usare gradle

---
+++ gradle_tutorial/src/test/java/tutorial/GradleTutorialTest.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+}
